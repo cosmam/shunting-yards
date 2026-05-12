983c96b2832ce477d5e56a0aa1c39972a4226a82
Travis Hicks <fadedstarlight@gmail.com>
2026-05-12 07:57:20 -0400

    Unit testing basic parsing of symbols from grammar

--- rust/shunting_yard/src/ast.rs
+++ rust/shunting_yard/src/ast.rs
@@ -111,5 +111,168 @@ mod tests {
             Ok(Box::new(Expression::Float(expected)))
         );
     }
+           
+    #[test]
+    fn test_parse_integer() {
+               let lexer = lexer::Lexer::new("146");
+        let parser = calc::ExpressionParser::new();
+        let mut errors = Vec::new();
+        let result = parser.parse(&mut errors, lexer);
+
+        assert_eq!(
+            result,
+            Ok(Box::new(Expression::Integer(146)))
+        );
+    }
+           
+    #[test]
+    fn test_parse_hex() {
+               let lexer = lexer::Lexer::new("0xfe");
+        let parser = calc::ExpressionParser::new();
+        let mut errors = Vec::new();
+        let result = parser.parse(&mut errors, lexer);
+
+        assert_eq!(
+            result,
+            Ok(Box::new(Expression::Integer(254)))
+        );
+    }
+           
+    #[test]
+    fn test_parse_variable() {
+               let lexer = lexer::Lexer::new("some_name[1]");
+        let parser = calc::ExpressionParser::new();
+        let mut errors = Vec::new();
+        let result = parser.parse(&mut errors, lexer);
+
+        assert_eq!(
+            result,
+            Ok(Box::new(Expression::Variable("some_name[1]".to_string())))
+        );
+    }
+
+    #[rstest]
+    #[case("==", Opcode::Equals)]
+    #[case("!=", Opcode::NotEquals)]
+    #[case("/=", Opcode::NotEquals)]
+    #[case("<=", Opcode::LessThanEquals)]
+    #[case(">=", Opcode::GreaterThanEquals)]
+    #[case("~=", Opcode::ApproximatelyEquals)]
+    #[case("**", Opcode::Power)]
+    #[case("*", Opcode::Multiply)]
+    #[case("/", Opcode::Divide)]
+    #[case("+", Opcode::Plus)]
+    #[case("-", Opcode::Minus)]
+    #[case("^", Opcode::BitwiseXor)]
+    #[case("%", Opcode::Modulo)]
+    #[case("&&", Opcode::LogicalAnd)]
+    #[case("||", Opcode::LogicalOr)]
+    #[case("<<", Opcode::BitshiftLeft)]
+    #[case(">>", Opcode::BitshiftRight)]
+    #[case("&", Opcode::BitwiseAnd)]
+    #[case("|", Opcode::BitwiseOr)]
+    #[case("<", Opcode::LessThan)]
+    #[case(">", Opcode::GreaterThan)]
+    fn test_parse_binary_operators(#[case] op_str: &str, #[case] expected: Opcode) {
+        let input = format!("1 {} 2", op_str);
+        let lexer = lexer::Lexer::new(&input);
+        let parser = calc::ExpressionParser::new();
+        let mut errors = Vec::new();
+        let result = parser.parse(&mut errors, lexer);
+
+        assert_eq!(
+            result,
+            Ok(Box::new(Expression::BinaryOperation {
+                lhs: Box::new(Expression::Integer(1)),
+                operator: expected,
+                rhs: Box::new(Expression::Integer(2))
+            }))
+        );
+    }
+
+    #[rstest]
+    #[case("~", Opcode::BitwiseNot)]
+    #[case("+", Opcode::Plus)]
+    #[case("-", Opcode::Minus)]
+    #[case("!", Opcode::LogicalNot)]
+    fn test_parse_unary_operators_left(#[case] op_str: &str, #[case] expected: Opcode) {
+        let input = format!("{}3.1", op_str);
+        let lexer = lexer::Lexer::new(&input);
+        let parser = calc::ExpressionParser::new();
+        let mut errors = Vec::new();
+        let result = parser.parse(&mut errors, lexer);
+
+        assert_eq!(
+            result,
+            Ok(Box::new(Expression::UnaryOperation {
+                operator: expected,
+                value: Box::new(Expression::Float(3.1))
+            }))
+        );
+    }
+
+    #[rstest]
+    #[case("°", Opcode::Degrees)]
+    fn test_parse_unary_operators_right(#[case] op_str: &str, #[case] expected: Opcode) {
+        let input = format!("3.1{}", op_str);
+        let lexer = lexer::Lexer::new(&input);
+        let parser = calc::ExpressionParser::new();
+        let mut errors = Vec::new();
+        let result = parser.parse(&mut errors, lexer);
+
+        assert_eq!(
+            result,
+            Ok(Box::new(Expression::UnaryOperation {
+                operator: expected,
+                value: Box::new(Expression::Float(3.1))
+            }))
+        );
+    }
+
+    // functions can take a list, but that's a different test
+    #[rstest]
+    #[case("min", Func::Min)]
+    #[case("max", Func::Max)]
+    #[case("pow", Func::Power)]
+    #[case("mod", Func::Modulo)]
+    #[case("rem", Func::Remainder)]
+    #[case("round", Func::Round)]
+    #[case("acos", Func::ACos)]
+    #[case("asin", Func::ASin)]
+    #[case("atan", Func::ATan)]
+    #[case("abs", Func::Abs)]
+    #[case("ln", Func::Ln)]
+    #[case("log", Func::Log)]
+    #[case("exp", Func::Exp)]
+    #[case("floor", Func::Floor)]
+    #[case("ceil", Func::Ceiling)]
+    #[case("ceiling", Func::Ceiling)]
+    #[case("cos", Func::Cos)]
+    #[case("sin", Func::Sin)]
+    #[case("tan", Func::Tan)]
+    fn test_parse_function_names(#[case] op_str: &str, #[case] expected: Func) {
+        let input = format!("{}(10)", op_str);
+        let lexer = lexer::Lexer::new(&input);
+        let parser = calc::ExpressionParser::new();
+        let mut errors = Vec::new();
+        let result = parser.parse(&mut errors, lexer);
+
+        let values: Vec<Box<Expression>> = vec![
+            Box::new(Expression::Integer(10)),
+        ];
+
+        assert_eq!(
+            result,
+            Ok(Box::new(Expression::Function {
+                func: expected, 
+                arguments: values,
+            }))
+        );
+    }
+
+    // associativity
+    // precedence
+    // expressions in function
+    // error conditions
 
 }
